dca144ce19b64ab79df240538233eade350fd6c3
Anna Stefaniv <happyasme11@gmail.com>
2025-03-23 20:32:11 -0700

    Refactored utils.js test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,16 +1,18 @@
 import { parseSender } from "../extension/background/utils.js";
 
-test("parseSender extracts name and email", () => {
-  const result = parseSender(["John Doe <john@example.com>", 5]);
-  expect(result).toEqual(["john@example.com", "John Doe", 5]);
-});
+describe("parseSender", () => {
+  test("extracts name and email", () => {
+    const result = parseSender(["John Doe <john@example.com>", 5]);
+    expect(result).toEqual(["john@example.com", "John Doe", 5]);
+  });
 
-test("parseSender extracts name and email with no name", () => {
-  const result = parseSender(["aaron@example.com", 8]);
-  expect(result).toEqual(["aaron@example.com", "aaron", 8]);
-});
+  test(" extracts name and email with no name", () => {
+    const result = parseSender(["aaron@example.com", 8]);
+    expect(result).toEqual(["aaron@example.com", "aaron", 8]);
+  });
 
-test("parseSender extracts name and email when error", () => {
-  const result = parseSender(["", 8]); // invalid sender
-  expect(result).toEqual(["", "", 8]);
+  test("extracts name and email when error", () => {
+    const result = parseSender(["", 8]); // invalid sender
+    expect(result).toEqual(["", "", 8]);
+  });
 });
