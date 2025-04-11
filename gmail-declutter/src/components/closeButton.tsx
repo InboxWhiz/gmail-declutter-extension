2710f1ea1bd841a1fae7ec2e009031169e1ce7d5
Anna Stefaniv <happyasme11@gmail.com>
2025-04-11 12:39:57 -0700

    Added close button functionality

--- gmail-declutter/src/components/closeButton.tsx
+++ gmail-declutter/src/components/closeButton.tsx
@@ -2,7 +2,14 @@ import './closeButton.css'
 
 export const CloseButton = () => {
 
+    const handleClick = () => {
+        const bodyElement: HTMLElement | null = document.getElementById("declutter-body");
+        if (bodyElement) {
+            bodyElement.style.display = "none";
+        }
+    };
+
     return (
-        <button className="close-button" aria-label="Close"></button>
+        <button className="close-button" aria-label="Close" onClick={handleClick}></button>
     )
 }
