64b8f32b5d9a211e048281b45ce92713093d7f34
Anna Stefaniv <happyasme11@gmail.com>
2025-04-13 15:22:37 -0700

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,14 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
     },
   },
 );
