8df7315b0a5af7404d12708fb04149931c6ba91e
Anna Stefaniv <happyasme11@gmail.com>
2025-04-20 17:50:26 -0700

    Added a toggleOption component to encompass entire toggle and label

---
+++ src/components/toggleOption.tsx
@@ -0,0 +1,22 @@
+import { ToggleSwitch } from "./toggleSwitch";
+
+export const ToggleOption = ({
+    label,
+    defaultChecked,
+    onChange,
+}: {
+    label: string | React.ReactElement
+    defaultChecked: boolean;
+    onChange: (checked: boolean) => void;
+}) => {
+    return (
+        <div className="toggle-option">
+            <ToggleSwitch
+                defaultChecked={defaultChecked}
+                onChange={onChange}
+            />
+            <div style={{ width: "10px"}}></div>
+            <p className="note">{label}</p>
+        </div>
+    );
+};
