9686a8d93ed63e3a623f190425a73fc334ad0e14
Anna Stefaniv <happyasme11@gmail.com>
2025-05-12 14:57:26 -0700

    Login page

--- src/sidebar/components/login-page/loginPage.tsx
+++ src/sidebar/components/login-page/loginPage.tsx
@@ -1,14 +1,24 @@
 import { GoogleAuthButton } from "./googleAuthButton.tsx";
+import { getAssetUrl } from "../../../_shared/utils/utils";
+import { useActions } from "../../providers/actionsContext.tsx";
+import { useEffect, useState } from "react";
 
 export function LoginPage() {
+  const { getEmailAccount } = useActions();
+  const [email, setEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    getEmailAccount().then(setEmail)
+  }, []);
+
   return (
-      <div id="declutter-body" className="login">
-        <div style={{ height: "70px" }}></div>
-        <img src="./assets/logo.svg" alt="InboxWhiz Logo" height="200px"/>
-        <span style={{ textAlign: "center"}}>Please sign in to your Google account<br/>to use InboxWhiz.</span>
-        <div style={{ height: "30px" }}></div>
-        <GoogleAuthButton onAuthSuccess={() => {}} />
-        <span id="email-account">happyasme11@gmail.com</span>
-      </div>
+    <div id="declutter-body" className="login">
+      <div style={{ height: "70px" }}></div>
+      <img src={getAssetUrl("assets/logo.svg")} alt="InboxWhiz Logo" height="200px" />
+      <span style={{ textAlign: "center" }}>Please sign in to your Google account<br />to use InboxWhiz.</span>
+      <div style={{ height: "30px" }}></div>
+      <GoogleAuthButton onAuthSuccess={() => { }} />
+      <span id="email-account">{email}</span>
+    </div>
   );
 }
