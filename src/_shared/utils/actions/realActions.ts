280d21b63a2b0ce466f7a3af1bdd258075efec05
Anna Stefaniv <happyasme11@gmail.com>
2025-05-26 11:31:58 -0700

    Moved getEmailAccount definition to utils

--- src/_shared/utils/actions/realActions.ts
+++ src/_shared/utils/actions/realActions.ts
@@ -12,6 +12,7 @@ import {
 } from "../unsubscribeSenders";
 import { Actions } from "./actionsInterface";
 import { getCachedToken, getValidToken, signInWithGoogle } from "../googleAuth";
+import { getEmailAccount } from "../utils";
 
 export const realActions: Actions = {
   async isLoggedIn(
@@ -28,33 +29,7 @@ export const realActions: Actions = {
 
   signInWithGoogle,
 
-  async getEmailAccount(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const tabId = tabs[0]?.id;
-        if (tabId === undefined) {
-          reject("No active tab.");
-          return;
-        }
-
-        chrome.tabs.sendMessage(
-          tabId,
-          { action: "GET_EMAIL_ACCOUNT" },
-          (response) => {
-            if (chrome.runtime.lastError) {
-              console.error(
-                "Could not get email account:",
-                chrome.runtime.lastError,
-              );
-              reject(chrome.runtime.lastError.message);
-            } else {
-              resolve(response.result);
-            }
-          },
-        );
-      });
-    });
-  },
+  getEmailAccount,
 
   searchEmailSenders(senderEmailAddresses: string[]): void {
     // Searches for emails in the Gmail tab
