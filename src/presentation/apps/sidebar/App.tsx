27ab42c3b1f55ed2a7b32f068d3bf4bda0b27de3
Anna Stefaniv Oickle <happyasme11@gmail.com>
2025-08-28 15:55:39 -0700

    No global provider

--- src/presentation/apps/sidebar/App.tsx
+++ src/presentation/apps/sidebar/App.tsx
@@ -5,38 +5,43 @@ import { ReloadButton } from "./components/reloadButton.tsx";
 import { ModalPopup } from "./components/modalPopup.tsx";
 import { SendersContainer } from "./components/sendersContainer.tsx";
 import { DeclutterHeader } from "./components/header.tsx";
-// import { useActions } from "../_shared/providers/actionsContext.tsx";
 import { ModalProvider } from "./providers/modalContext.tsx";
-import { ThemeContext } from "../../../_shared/providers/themeContext.ts";
 import ThemeToggle from "./components/themeToggle.tsx";
 import { AppProvider } from "../../providers/app_provider.tsx";
+import { ThemeProvider } from "../../providers/ThemeProvider.tsx";
+import { ActionsProvider } from "../../../_shared/providers/actionsContext.tsx";
+import { ThemeContext } from "../../providers/themeContext.ts";
 
 function App() {
   const { theme } = useContext(ThemeContext);
 
   return (
     <AppProvider>
-      <ModalProvider>
-        <div id="declutter-body" className={theme}>
-          <DeclutterHeader />
+      <ActionsProvider>
+        <ThemeProvider>
+          <ModalProvider>
+            <div id="declutter-body" className={theme}>
+              <DeclutterHeader />
 
-          <div className="button-bar">
-            <div className="sender-actions">
-              <ActionButton id="unsubscribe-button" />
-              <ActionButton id="delete-button" />
+              <div className="button-bar">
+                <div className="sender-actions">
+                  <ActionButton id="unsubscribe-button" />
+                  <ActionButton id="delete-button" />
+                </div>
+
+                <div style={{ display: "flex" }}>
+                  <ReloadButton />
+                  <ThemeToggle />
+                </div>
+              </div>
+
+              <SendersContainer />
+
+              <ModalPopup />
             </div>
-
-            <div style={{ display: "flex" }}>
-              <ReloadButton />
-              <ThemeToggle />
-            </div>
-          </div>
-
-          <SendersContainer />
-
-          <ModalPopup />
-        </div>
-      </ModalProvider>
+          </ModalProvider>
+        </ThemeProvider>
+      </ActionsProvider>
     </AppProvider>
   );
 }
